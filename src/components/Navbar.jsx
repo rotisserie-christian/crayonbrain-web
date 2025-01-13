0cd6853153a1e996dc3864766ba3b395c669109d
Christian Waters <102264428+rotisserie-christian@users.noreply.github.com>
2025-01-13 05:27:46 -0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,11 @@ const Navbar = () => {
         <div className="max-w-6xl mx-auto px-4">
           <div className="flex items-center justify-between">
             <div className='flex flex-row items-center gap-1'>
-              <p className='text-xl courier-new'>Crayonbrain</p>
+              <p className='text-lg courier-new'>Crayonbrain</p>
             </div>
 
             <button
-            className='btn btn-primary rounded-xl'
+            className='btn btn-primary btn-sm rounded-xl'
             >Log in</button>
           </div>
         </div>
